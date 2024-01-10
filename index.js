029c29dba36483f0f989f84526e444bce272a0a5
Alaincustic <147705200+Alaincustic@users.noreply.github.com>
2024-01-10 08:34:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get(
     }
   },
   (req, res) => {
-    res.sendFile(path.join(__dirname, "alt.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
     // res.send(`<iframe width="100%" height="100%" margin-top:"30%" src="https://www.youtube.com/embed/463tZXEDhig?si=okMgnV6S1RF1XDhN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
   }
 );
