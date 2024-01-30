de231925d561a39b7632145f7f7ed28d6deb96f2
Alaincustic <147705200+Alaincustic@users.noreply.github.com>
2024-01-30 11:41:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,72 +8,68 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 // List of allowed frontend origins for CORS
-// const allowedOrigins = [
-//   "http://127.0.0.1:5501",
-//   "http://127.0.0.0:5501",
-//   "https://carsworldjapan.shop",
-//   "http://carsworldjapan.shop",
-//   "https://japanpopup.netlify.app",
-//   "https://japanpopup.netlify.app/#",
-// ];
+const allowedOrigins = [
+  "http://127.0.0.1:5501",
+  "http://127.0.0.0:5501",
+  "https://heiwajima.in",
+  "http://heiwajima.in",
+];
 
-// // List of allowed referrers
-// const allowedReferrers = [
-//   "http://127.0.0.1:5501",
-//   "http://127.0.0.0:5501",
-//   "https://carsworldjapan.shop",
-//   "http://carsworldjapan.shop",
-//   "https://japanpopup.netlify.app",
-//   "https://japanpopup.netlify.app/#",
-// ];
+// List of allowed referrers
+const allowedReferrers = [
+  "http://127.0.0.1:5501",
+  "http://127.0.0.0:5501",
+  "https://heiwajima.in",
+  "http://heiwajima.in",
+];
 
-// // CORS configuration
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
+// CORS configuration
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-// // Apply the CORS middleware
-// app.use(cors(corsOptions));
+// Apply the CORS middleware
+app.use(cors(corsOptions));
 
-// // Check against the allowedReferrers
-// app.get(
-//   "/",
-//   (req, res, next) => {
-//     const referer = req.headers.referer;
+// Check against the allowedReferrers
+app.get(
+  "/",
+  (req, res, next) => {
+    const referer = req.headers.referer;
 
-//     // Check if the referer exists in the allowedReferrers array
-//     if (
-//       referer &&
-//       allowedReferrers.some((domain) => referer.startsWith(domain))
-//     ) {
-//       next();
-//     } else {
-//       res.status(403).send("Access forbidden");
-//     }
-//   },
-//   (req, res) => {
-//     res.sendFile(path.join(__dirname, "index.html"));
-//     // res.send(`<iframe width="100%" height="100%" margin-top:"30%" src="https://www.youtube.com/embed/463tZXEDhig?si=okMgnV6S1RF1XDhN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
-//   }
-// );
+    // Check if the referer exists in the allowedReferrers array
+    if (
+      referer &&
+      allowedReferrers.some((domain) => referer.startsWith(domain))
+    ) {
+      next();
+    } else {
+      res.status(403).send("Access forbidden");
+    }
+  },
+  (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+    // res.send(`<iframe width="100%" height="100%" margin-top:"30%" src="https://www.youtube.com/embed/463tZXEDhig?si=okMgnV6S1RF1XDhN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
+  }
+);
 
-app.use(cors());
+// app.use(cors());
 
-app.post("/submit-phone", (req, res) => {
-  // Log the phone number on the server side
-  console.log("Received phone number:", req.body);
+// app.post("/submit-phone", (req, res) => {
+//   // Log the phone number on the server side
+//   console.log("Received phone number:", req.body);
 
-  res.header("Access-Control-Allow-Origin", "*"); // Allow CORS from any origin
+//   res.header("Access-Control-Allow-Origin", "*"); // Allow CORS from any origin
 
-  // Send a response back to the client
-  res.json({ message: "Phone number submitted successfully" });
-});
+//   // Send a response back to the client
+//   res.json({ message: "Phone number submitted successfully" });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running`);
