98522f949312a3265b8b77267d1e1b38882bd001
Alaincustic <147705200+Alaincustic@users.noreply.github.com>
2024-02-03 20:37:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ app.use(bodyParser.json());
 
 // List of allowed frontend origins for CORS
 const allowedOrigins = [
-  "https://heiwajima.in/",
-  "http://heiwajima.in/",
-  "https://heiwajima.in",
-  "http://heiwajima.in",
+  "https://thaibasiljp.com/",
+  "http://thaibasiljp.com/",
+  "https://thaibasiljp.com",
+  "http://thaibasiljp.com",
   "http://127.0.0.1:5501",
   "http://127.0.0.1:5500",
 ];
 
 // List of allowed referrers
 const allowedReferrers = [
-  "https://heiwajima.in/",
-  "http://heiwajima.in/",
-  "https://heiwajima.in",
-  "http://heiwajima.in",
+  "https://thaibasiljp.com/",
+  "http://thaibasiljp.com/",
+  "https://thaibasiljp.com",
+  "http://thaibasiljp.com",
   "http://127.0.0.1:5501",
   "http://127.0.0.1:5500",
 ];
